fdd7ebbd5754db3974ac57706533d687d0045a27
Hauke Bartsch <HaukeBartsch@gmail.com>
2024-09-10 09:55:16 +0200

    add some more thoughts

--- README.md
+++ README.md
@@ -15,4 +15,25 @@ Approach 1: Based on the product of the machine precision and the largest singul
 
 Approach 2: Better to use a Tikhonov regularization. To compute the inverse of S we use diagonal elements in S that are $\frac{\sigma_i}{\sigma_i^2 + \alpha^2}$ where $\sigma$ are the singular values. Given some $\alpha$ (0.001?) this will slowly penalize values that are small.
 
+> [!NOTE]
+> You can see how the regularization works if you ignore $\alpha$, remove a $\sigma$ from above and below the fraction and you get one over $\sigma$.
+
+
 ![distance as color background with point-cloud](https://github.com/HaukeBartsch/outliers/blob/main/images/distance_overlay.png)
+
+## Observations
+
+The example in the code is two dimensional but the approach natually extends to N>2 dimensions.
+
+Numeric valued data are only one category. There are also categorical/factor level variables (is-female) and ordinal value (Likert-scale).
+
+Many datasets are far away from being normal (Gaussian, defined by first and second order moments only), they can be skewed or have unusual kurtosis (peakedness or heavy tails). Maybe we can use a transformation of those dimensions first?
+
+The Mahalanobis distance ignores any higher order moments (>2), it relies on correlations only. What about statistical independence? 
+
+### How to use
+
+One idea is to rank data points. Sort datapoints using their Mahalanobis distance from largest to smallest. The hope is that points that appear early in this list are 'different' from most other points. Basically this works if your distribution is normal. Most points will be close to 0.
+
+> [!NOTE]
+> Notice that de-correlation does not rotate our distribution? The eigenvectors will point into some usfull directions but because we just want to compute a scalar distance we ignore that information.
--- mahalanobis.m
+++ mahalanobis.m
@@ -1,61 +1,78 @@
+#
 # The following code has been tested on Octave.
+#
 
-# x and y as iid normal 2D distribution
+# create x and y as iid normal 2D distribution
 x = randn (1000, 1);
 y = randn (1000, 1);
+# We believe the above and do not plot - but we could.
 # scatter (xt, yt);
 
-# an example random covariance matrix (symmetric, positive definite)
+# An example random covariance matrix A (symmetric, positive definite).
 A = randn(2,2); A = A'*A; A = (A + A')/2;
 
-# distort our centered normal distribution using A
+# Distort our centered normal distribution using A
 t = A*[x, y]';
 xt = t(1,:)';
 yt = t(2,:)';
-# and plot the result
+# and plot the result:
 scatter (xt, yt);
-# compute its covariance matrix
+# The plot often reverts to a single long line with some random orientation. Do this
+# a couple of times and you will end up with one that is a wider elipsoid.
+
+# Compute the covariance matrix of this 2d distribution.
 C = cov(xt, yt);
 
-# Now we can use a new point np and compute the Mahalanobis distance to our distribution
+# Now we can use a new 2d-point np and compute the Mahalanobis distance to our distribution.
 np = [0,10];
 
 #
-# Use SVD to compute singular values of the variance-covariance matrix
-#
+# Use SVD to compute singular value decomposition of the variance-covariance matrix.
 [U, S, V] = svd(C);
-# Now C equals U*S*V'
+# Now C equals U*S*V'.
 # Replace the sigular values with their inverses, using Tikhonov regularization
 # Instead of Sn = diag(1./diag(S)) use regularization:
 Sn = diag( diag(S) ./ (diag(S).^2 + 0.001^2) )
 
+# Reconstitute a C using the changed/inverted singular values. Applying this Ct matrix to a datapoint
+# will change the datapoint, large values are getting smaller and small values are getting larger. 
+# This does not rotate our distribution but makes the two axes/dimensions have the same variance - we 
+# de-correlate the data in [xt; yt].
 Ct = U*Sn*V';
-# Ct represents the 'space-distortion' of our distribution of points.
 # Compute the Mahalanobis distance mdist of np to C using a quadratic form.
 mdist = np * Ct * np'
+# The above "quadratic form" transforms np by Ct, decorrelating it, np is moved to undo the 
+# distortion caused by the correlation of the data. We compute a sum of squares as a distance 
+# to the origin in that decorrelated space. 'mdist' is now a scalar distance value associated 
+# with np. Large mdist means a large Mahalanobis distance of np to the center of the distribution
+# (first order) and based on the variances (second order moments).
 
-# Here an example. A could be:
+#
+# Here an example. A nice $A$ could be:
+#
 A = [ 2.4612, 1.3870; 1.3870, 1.2047];
-# The point cloud would look like this:
+# The point cloud looks like this:
 t = A*[x, y]';
 xt = t(1,:)';
 yt = t(2,:)';
 scatter (xt, yt);
-# and have a covariance matrix:
+# and has a variance/covariance matrix:
 C = cov(xt, yt);
-# The Mahalanobis computation again:
+# The Mahalanobis computation using Tikhonov regularization:
 [U, S, V] = svd(C);
 Sn = diag( (diag(S) ./ (diag(S).^2 + 0.001^2)) );
 Ct = U*Sn*V';
 
-# And compute one scalar distance to point [5,2] (close to the distribution)
+# And compute one scalar distance $d$ to point [5,2] (close to the distribution)
 np = [5,2];
 d = np * Ct * np'
 # And now a point [-5,2] (farther away from the distribution)
 np = [-5,2];
 d = np * Ct * np'
 
+#
 # Distance map to verify what we are doing.
+#
 [mx,my] = meshgrid(-10:1:10, -10:1:10);
 m = zeros(size(mx,1),size(mx,2))
 for i=1:size(mx,1)
@@ -67,3 +84,4 @@ end
 pcolor (mx, my, m); colorbar; colormap(gray);
 hold on;
 scatter (xt, yt);
+# In this image large values are farther away from the distribution.
